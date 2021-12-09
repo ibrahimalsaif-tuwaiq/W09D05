67fb566ecf22d49c6c43fb157177340b2817c4db
ibrahimalsaif-tuwaiq <Ibrahim.Husain.elq.js@tuwaiq.edu.sa>
2021-12-09 16:37:51 +0300

    signup page

---
+++ src/components/Signup/index.js
@@ -0,0 +1,117 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import PasswordChecklist from "react-password-checklist";
+import axios from "axios";
+import "./style.css";
+
+const Signup = () => {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const userStorage = localStorage.getItem("user");
+    setUser(JSON.parse(userStorage));
+  }, []);
+
+  const signup = async () => {
+    setMessage("");
+    const res = await axios.post(`${process.env.REACT_APP_BASE_URL}/signup`, {
+      username: username,
+      email: email,
+      password: password,
+      role: process.env.REACT_APP_USER_ROLE,
+    });
+    if (res.status === 201) {
+      navigate("/login");
+    } else {
+      setMessage(res.data.message);
+    }
+  };
+
+  return (
+    <div className="signupWrapper">
+      {user ? (
+        <h1>
+          <div className="centerWrapper">
+            <div className="homeSignupTitle">
+              <h1>You already loggedin, you don't need to signup</h1>
+            </div>
+            <div className="homeSignupButtons">
+              <button onClick={() => navigate("/")}>HOME</button>
+            </div>
+          </div>
+        </h1>
+      ) : (
+        <main className="signupPanel">
+          <div className="signupPanel__half signupHalf--first">
+            <PasswordChecklist
+              rules={[
+                "minLength",
+                "specialChar",
+                "number",
+                "capital",
+                "lowercase",
+              ]}
+              minLength={6}
+              value={password}
+              onChange={(isValid) => {
+                if (isValid) {
+                  const button = document.querySelector("#signupSubmitButton");
+                  button.disabled = false;
+                } else {
+                  const button = document.querySelector("#signupSubmitButton");
+                  button.disabled = true;
+                }
+              }}
+            />
+            <button id="loginButton" onClick={() => navigate("/login")}>
+              or go to login
+            </button>
+          </div>
+          <div className="signupPanel__half signupHalf--second">
+            <h2>Signup</h2>
+            {message ? <div className="message">{message}</div> : ""}
+            <form
+              className="signupInput"
+              onSubmit={(e) => {
+                e.preventDefault();
+                signup(e);
+              }}
+            >
+              <input
+                type="text"
+                placeholder="Username"
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+              <input
+                type="text"
+                placeholder="Email"
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+              <input
+                id="signupSubmitButton"
+                type="submit"
+                value="Submit"
+                disabled
+              />
+            </form>
+          </div>
+        </main>
+      )}
+    </div>
+  );
+};
+
+export default Signup;
